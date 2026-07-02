af503e0dc1a28be824ead84f8908a3348221f582
Jim56450 <jmd.range@gmail.com>
2026-07-02 16:38:00 +0200

    Scan local module update zips

--- src/Browser/BrowserWindowController.mm
+++ src/Browser/BrowserWindowController.mm
@@ -49,6 +49,7 @@ static NSString* const kMainWindowFrameDefaultsKey = @"MainWindowFrame";
 static NSString* const kMainWindowZoomedDefaultsKey = @"MainWindowZoomed";
 static NSString* const kModuleUpdateURLDefaultsKey = @"ModuleUpdateURL";
 static NSString* const kModuleUpdateLocalDirectoryDefaultsKey = @"ModuleUpdateLocalDirectory";
+static NSString* const kModuleUpdateLocalIndexFilename = @"module-update-local-index.json";
 static NSString* const kDeveloperToolsDockModeBottom = @"bottom";
 static NSString* const kDeveloperToolsDockModeTop = @"top";
 static NSString* const kDeveloperToolsDockModeLeft = @"left";
@@ -3991,7 +3992,7 @@ class BabelReloadIgnoreCacheCallback final : public CefCompletionCallback {
   } else if (manifest.count == 0) {
     NSString* message = errorMessage.length > 0
         ? errorMessage
-        : @"Configure an update URL or a local update folder containing modules-release-manifest.json.";
+        : @"Configure an update URL or a local update folder containing module zips.";
     [rowsHTML appendFormat:@"<p class='empty'>%@</p>", [self htmlEscapedString:message]];
   } else if (installedModules.count == 0) {
     [rowsHTML appendString:@"<p class='empty'>No installed module was found.</p>"];
@@ -4161,7 +4162,7 @@ class BabelReloadIgnoreCacheCallback final : public CefCompletionCallback {
     }
     return @{
       @"sourceLabel" : [NSString stringWithFormat:@"Local fallback: %@", localDirectory],
-      @"error" : error.localizedDescription ?: @"Unable to read the local update manifest."
+      @"error" : error.localizedDescription ?: @"Unable to read the local update folder."
     };
   }
 
@@ -4208,24 +4209,214 @@ class BabelReloadIgnoreCacheCallback final : public CefCompletionCallback {
 }
 
 - (NSDictionary*)releaseManifestResultFromLocalPath:(NSString*)path error:(NSError**)error {
-  NSString* manifestPath = [self moduleUpdateManifestPathForLocalPath:path];
-  NSData* data = [NSData dataWithContentsOfFile:manifestPath options:0 error:error];
-  if (!data) {
+  BOOL isDirectory = NO;
+  if (![NSFileManager.defaultManager fileExistsAtPath:path isDirectory:&isDirectory] || !isDirectory) {
+    if (error) {
+      *error = [NSError errorWithDomain:@"fr.babelforge.babel-chrome.modules"
+                                   code:6
+                               userInfo:@{NSLocalizedDescriptionKey : @"The local update source must be a folder containing module zips."}];
+    }
     return nil;
   }
 
-  NSDictionary* manifest = [self releaseManifestFromData:data error:error];
-  if (!manifest) {
+  NSArray* directoryEntries = [NSFileManager.defaultManager contentsOfDirectoryAtPath:path error:error];
+  if (!directoryEntries) {
     return nil;
   }
 
-  NSString* basePath = manifestPath.stringByDeletingLastPathComponent ?: @"";
+  NSDictionary* cachedIndex = [self moduleUpdateLocalIndex];
+  NSDictionary* cachedItems = [cachedIndex[@"items"] isKindOfClass:NSDictionary.class] ? cachedIndex[@"items"] : @{};
+  NSMutableDictionary* nextCachedItems = [NSMutableDictionary dictionary];
+  NSMutableDictionary* latestModulesByIdentifier = [NSMutableDictionary dictionary];
+
+  for (NSString* entryName in directoryEntries) {
+    if (![entryName isKindOfClass:NSString.class] || ![entryName.pathExtension.lowercaseString isEqualToString:@"zip"]) {
+      continue;
+    }
+
+    NSString* zipPath = [path stringByAppendingPathComponent:entryName];
+    NSDictionary* releaseModule = [self releaseModuleFromLocalZipAtPath:zipPath
+                                                               fileName:entryName
+                                                            cachedItems:cachedItems];
+    if (!releaseModule) {
+      continue;
+    }
+
+    nextCachedItems[zipPath] = releaseModule;
+    NSString* moduleIdentifier = [releaseModule[@"id"] isKindOfClass:NSString.class] ? releaseModule[@"id"] : @"";
+    if (moduleIdentifier.length == 0) {
+      continue;
+    }
+
+    NSDictionary* currentModule = latestModulesByIdentifier[moduleIdentifier];
+    if (!currentModule || [self shouldPreferReleaseModule:releaseModule overReleaseModule:currentModule]) {
+      latestModulesByIdentifier[moduleIdentifier] = releaseModule;
+    }
+  }
+
+  [self writeModuleUpdateLocalIndex:@{
+    @"version" : @1,
+    @"items" : nextCachedItems
+  }];
+
+  NSArray* modules = [latestModulesByIdentifier.allValues sortedArrayUsingComparator:^NSComparisonResult(NSDictionary* leftModule,
+                                                                                                         NSDictionary* rightModule) {
+    NSString* leftIdentifier = [leftModule[@"id"] isKindOfClass:NSString.class] ? leftModule[@"id"] : @"";
+    NSString* rightIdentifier = [rightModule[@"id"] isKindOfClass:NSString.class] ? rightModule[@"id"] : @"";
+    return [leftIdentifier compare:rightIdentifier options:NSCaseInsensitiveSearch];
+  }];
+  if (modules.count == 0) {
+    if (error) {
+      *error = [NSError errorWithDomain:@"fr.babelforge.babel-chrome.modules"
+                                   code:7
+                               userInfo:@{NSLocalizedDescriptionKey : @"No valid module zip was found in the local update folder."}];
+    }
+    return nil;
+  }
+
+  NSDictionary* manifest = @{
+    @"generatedAt" : [NSDate.date descriptionWithLocale:nil],
+    @"modules" : modules
+  };
   return @{
     @"manifest" : manifest,
     @"sourceKind" : @"local",
-    @"sourceLabel" : [NSString stringWithFormat:@"Local: %@", manifestPath],
-    @"basePath" : basePath
+    @"sourceLabel" : [NSString stringWithFormat:@"Local folder: %@", path],
+    @"basePath" : path
   };
+}
+
+- (NSDictionary*)releaseModuleFromLocalZipAtPath:(NSString*)zipPath
+                                        fileName:(NSString*)fileName
+                                     cachedItems:(NSDictionary*)cachedItems {
+  NSDictionary* attributes = [NSFileManager.defaultManager attributesOfItemAtPath:zipPath error:nil];
+  NSDate* modificationDate = [attributes[NSFileModificationDate] isKindOfClass:NSDate.class]
+      ? attributes[NSFileModificationDate]
+      : NSDate.distantPast;
+  NSNumber* fileSize = [attributes[NSFileSize] isKindOfClass:NSNumber.class] ? attributes[NSFileSize] : @0;
+  NSNumber* modifiedAt = @((NSInteger)floor(modificationDate.timeIntervalSince1970));
+
+  NSDictionary* cachedItem = [cachedItems[zipPath] isKindOfClass:NSDictionary.class] ? cachedItems[zipPath] : nil;
+  if (cachedItem &&
+      [cachedItem[@"filemtime"] isEqual:modifiedAt] &&
+      [cachedItem[@"size"] isEqual:fileSize] &&
+      [cachedItem[@"id"] isKindOfClass:NSString.class] &&
+      [cachedItem[@"version"] isKindOfClass:NSString.class]) {
+    return cachedItem;
+  }
+
+  NSError* manifestError = nil;
+  NSDictionary* moduleManifest = [self moduleManifestFromZipAtPath:zipPath error:&manifestError];
+  if (!moduleManifest) {
+    NSLog(@"Unable to read module update zip manifest at %@: %@", zipPath, manifestError.localizedDescription);
+    return nil;
+  }
+
+  NSString* moduleIdentifier = [moduleManifest[@"id"] isKindOfClass:NSString.class] ? moduleManifest[@"id"] : @"";
+  NSString* moduleVersion = [moduleManifest[@"version"] isKindOfClass:NSString.class] ? moduleManifest[@"version"] : @"";
+  if (moduleIdentifier.length == 0 || moduleVersion.length == 0) {
+    NSLog(@"Skipping module update zip without id or version: %@", zipPath);
+    return nil;
+  }
+
+  NSString* moduleName = [moduleManifest[@"name"] isKindOfClass:NSString.class] ? moduleManifest[@"name"] : moduleIdentifier;
+  NSMutableDictionary* releaseModule = [NSMutableDictionary dictionaryWithDictionary:moduleManifest];
+  releaseModule[@"id"] = moduleIdentifier;
+  releaseModule[@"name"] = moduleName;
+  releaseModule[@"version"] = moduleVersion;
+  releaseModule[@"zip"] = fileName ?: zipPath.lastPathComponent;
+  releaseModule[@"path"] = zipPath;
+  releaseModule[@"filemtime"] = modifiedAt;
+  releaseModule[@"size"] = fileSize;
+  return releaseModule;
+}
+
+- (BOOL)shouldPreferReleaseModule:(NSDictionary*)candidateModule overReleaseModule:(NSDictionary*)currentModule {
+  NSString* candidateVersion = [candidateModule[@"version"] isKindOfClass:NSString.class] ? candidateModule[@"version"] : @"";
+  NSString* currentVersion = [currentModule[@"version"] isKindOfClass:NSString.class] ? currentModule[@"version"] : @"";
+  NSComparisonResult versionComparison = [self compareVersion:candidateVersion toVersion:currentVersion];
+  if (versionComparison == NSOrderedDescending) {
+    return YES;
+  }
+  if (versionComparison == NSOrderedAscending) {
+    return NO;
+  }
+
+  NSNumber* candidateModifiedAt = [candidateModule[@"filemtime"] isKindOfClass:NSNumber.class] ? candidateModule[@"filemtime"] : @0;
+  NSNumber* currentModifiedAt = [currentModule[@"filemtime"] isKindOfClass:NSNumber.class] ? currentModule[@"filemtime"] : @0;
+  return [candidateModifiedAt compare:currentModifiedAt] == NSOrderedDescending;
+}
+
+- (NSDictionary*)moduleManifestFromZipAtPath:(NSString*)zipPath error:(NSError**)error {
+  NSData* manifestData = [self dataFromZipAtPath:zipPath innerPath:@"manifest.json" error:nil];
+  if (!manifestData) {
+    manifestData = [self dataFromZipAtPath:zipPath innerPath:@"*/manifest.json" error:error];
+  }
+  if (!manifestData) {
+    return nil;
+  }
+
+  return [self releaseManifestFromData:manifestData error:error];
+}
+
+- (NSData*)dataFromZipAtPath:(NSString*)zipPath innerPath:(NSString*)innerPath error:(NSError**)error {
+  NSTask* task = [[NSTask alloc] init];
+  task.executableURL = [NSURL fileURLWithPath:@"/usr/bin/unzip"];
+  task.arguments = @[@"-p", zipPath, innerPath];
+
+  NSPipe* outputPipe = [NSPipe pipe];
+  NSPipe* errorPipe = [NSPipe pipe];
+  task.standardOutput = outputPipe;
+  task.standardError = errorPipe;
+
+  NSError* launchError = nil;
+  if (![task launchAndReturnError:&launchError]) {
+    if (error) {
+      *error = launchError;
+    }
+    return nil;
+  }
+
+  NSData* outputData = [outputPipe.fileHandleForReading readDataToEndOfFile];
+  NSData* errorData = [errorPipe.fileHandleForReading readDataToEndOfFile];
+  [task waitUntilExit];
+  if (task.terminationStatus != 0 || outputData.length == 0) {
+    if (error) {
+      NSString* unzipError = [[NSString alloc] initWithData:errorData encoding:NSUTF8StringEncoding] ?: @"";
+      *error = [NSError errorWithDomain:@"fr.babelforge.babel-chrome.modules"
+                                   code:8
+                               userInfo:@{NSLocalizedDescriptionKey : unzipError.length > 0
+                                                                    ? unzipError
+                                                                    : @"Unable to extract manifest.json from the module zip."}];
+    }
+    return nil;
+  }
+
+  return outputData;
+}
+
+- (NSDictionary*)moduleUpdateLocalIndex {
+  NSString* indexPath = [self moduleUpdateLocalIndexPath];
+  NSData* data = [NSData dataWithContentsOfFile:indexPath];
+  if (!data) {
+    return @{};
+  }
+
+  id decoded = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+  return [decoded isKindOfClass:NSDictionary.class] ? decoded : @{};
+}
+
+- (void)writeModuleUpdateLocalIndex:(NSDictionary*)index {
+  NSString* indexPath = [self moduleUpdateLocalIndexPath];
+  NSString* indexDirectory = indexPath.stringByDeletingLastPathComponent;
+  [NSFileManager.defaultManager createDirectoryAtPath:indexDirectory
+                          withIntermediateDirectories:YES
+                                           attributes:nil
+                                                error:nil];
+  NSData* data = [NSJSONSerialization dataWithJSONObject:index options:NSJSONWritingPrettyPrinted error:nil];
+  if (data) {
+    [data writeToFile:indexPath options:NSDataWritingAtomic error:nil];
+  }
 }
 
 - (NSDictionary*)releaseManifestFromData:(NSData*)data error:(NSError**)error {
@@ -4342,13 +4533,8 @@ class BabelReloadIgnoreCacheCallback final : public CefCompletionCallback {
                                                          separator]];
 }
 
-- (NSString*)moduleUpdateManifestPathForLocalPath:(NSString*)path {
-  BOOL isDirectory = NO;
-  if ([NSFileManager.defaultManager fileExistsAtPath:path isDirectory:&isDirectory] && !isDirectory) {
-    return path;
-  }
-
-  return [path stringByAppendingPathComponent:@"modules-release-manifest.json"];
+- (NSString*)moduleUpdateLocalIndexPath {
+  return [BabelChromeConfiguration.applicationSupportDirectoryURL.path stringByAppendingPathComponent:kModuleUpdateLocalIndexFilename];
 }
 
 - (NSComparisonResult)compareVersion:(NSString*)leftVersion toVersion:(NSString*)rightVersion {
